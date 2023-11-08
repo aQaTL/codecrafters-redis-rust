5b840906bee4f5b95e6ee50730ed843ce6a77a0b
aQaTL <mmsoltys@outlook.com>
2023-11-08 23:13:40 +0100

    GET and SET

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::borrow::Cow;
+use std::collections::HashMap;
 use std::io::{self, Read, Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::sync::{Arc, RwLock};
 
 fn main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,10 +11,13 @@ fn main() {
 
 	let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+	let db: Arc<Db> = Arc::default();
+
 	for stream in listener.incoming() {
 		match stream {
 			Ok(stream) => {
-				std::thread::spawn(move || handle_connection(stream));
+				let db = Arc::clone(&db);
+				std::thread::spawn(move || handle_connection(stream, db));
 			}
 			Err(e) => {
 				println!("error: {}", e);
@@ -21,7 +26,12 @@ fn main() {
 	}
 }
 
-fn handle_connection(mut stream: TcpStream) {
+#[derive(Default)]
+struct Db {
+	hm: RwLock<HashMap<String, RESPMsg<'static>>>,
+}
+
+fn handle_connection(mut stream: TcpStream, db: Arc<Db>) {
 	let mut input = vec![0_u8; 256];
 	loop {
 		unsafe {
@@ -40,39 +50,80 @@ fn handle_connection(mut stream: TcpStream) {
 		println!("input: {message:#?}");
 
 		match message {
-			RESPMsg::Array(RESPArray(arr)) => {
-				match &arr[0] {
-					RESPMsg::BulkString(BulkString(str)) => {
-						match str as &str {
-							"ECHO" | "echo" => {
-								let RESPMsg::BulkString(payload) = &arr[1] else {
-									panic!();
-								};
-
-								// Echo back the same thing
-								payload.write_to(&mut stream).unwrap();
-							}
-							"ping" => {
-								PONG_RESPONSE.write_to(&mut stream).unwrap();
-							}
-							_ => panic!(),
-						}
+			RESPMsg::Array(RESPArray(arr)) => match &arr[0] {
+				RESPMsg::BulkString(BulkString(str)) => {
+					let command = str.to_ascii_lowercase();
+					match command.as_str() {
+						"echo" => handle_echo(&mut stream, &arr[1]),
+						"ping" => handle_ping(&mut stream),
+						"set" => handle_set(&mut stream, &db, &arr[1..]),
+						"get" => handle_get(&mut stream, &db, &arr[1..]),
+						_ => panic!("unknown command: {command}"),
 					}
-					RESPMsg::SimpleString(SimpleString(str)) => {
-						if str != "PING" {
-							panic!();
-						}
-						PONG_RESPONSE.write_to(&mut stream).unwrap();
-					}
-					_ => panic!(),
 				}
-			}
+				RESPMsg::SimpleString(SimpleString(str)) if str.eq_ignore_ascii_case("ping") => {
+					handle_ping(&mut stream);
+				}
+				_ => panic!(),
+			},
 			_ => unimplemented!(),
 		}
 
 		stream.flush().unwrap();
 	}
 	println!("closing connection");
+}
+
+fn handle_ping(stream: &mut TcpStream) {
+	PONG_RESPONSE.write_to(stream).unwrap();
+}
+
+fn handle_echo(stream: &mut TcpStream, payload: &RESPMsg<'_>) {
+	let RESPMsg::BulkString(payload) = payload else {
+		panic!();
+	};
+
+	// Echo back the same thing
+	payload.write_to(stream).unwrap();
+}
+
+fn handle_set(stream: &mut TcpStream, db: &Arc<Db>, payload: &[RESPMsg<'_>]) {
+	if payload.len() > 2 {
+		eprintln!("[WARN] Received more than <key> <value>. Ignoring that.");
+	}
+	let RESPMsg::BulkString(BulkString(key)) = &payload[0] else {
+		panic!();
+	};
+	let value = payload[1].clone();
+
+	{
+		db.hm.write().unwrap().insert(key.to_string(), value);
+	}
+
+	let response = SimpleString("OK".into());
+	response.write_to(stream).unwrap();
+}
+
+fn handle_get(stream: &mut TcpStream, db: &Arc<Db>, payload: &[RESPMsg<'_>]) {
+	if payload.len() > 1 {
+		eprintln!("[WARN] Received more than <key>. Ignoring that.");
+	}
+	let RESPMsg::BulkString(BulkString(key)) = &payload[0] else {
+		panic!();
+	};
+
+	let db = db.hm.read().unwrap();
+	let value = db.get(key as &str);
+	let response = match value {
+		Some(
+			RESPMsg::BulkString(BulkString(value)) | RESPMsg::SimpleString(SimpleString(value)),
+		) => RESPMsg::BulkString(BulkString(Cow::Borrowed(value))),
+		None => RESPMsg::Null,
+		_ => panic!(),
+	};
+
+	response.write_to(stream).unwrap();
+	drop(db);
 }
 
 const PONG_RESPONSE: SimpleString<'static> = SimpleString(Cow::Borrowed("PONG"));
@@ -82,6 +133,7 @@ enum RESPMsg<'a> {
 	SimpleString(SimpleString<'a>),
 	BulkString(BulkString<'a>),
 	Array(RESPArray<'a>),
+	Null,
 }
 
 impl RESPMsg<'_> {
@@ -91,6 +143,10 @@ impl RESPMsg<'_> {
 			RESPMsg::SimpleString(v) => v.write_to(w),
 			RESPMsg::BulkString(v) => v.write_to(w),
 			RESPMsg::Array(v) => v.write_to(w),
+			RESPMsg::Null => {
+				w.write_all(b"_\r\n")?;
+				Ok(3)
+			}
 		}
 	}
 
@@ -107,7 +163,20 @@ impl RESPMsg<'_> {
 			b'*' => RESPMsg::Array(RESPArray::decode(input)),
 			b'$' => RESPMsg::BulkString(BulkString::decode(input)),
 			b'+' => RESPMsg::SimpleString(SimpleString::decode(input)),
+			b'_' => RESPMsg::Null,
 			v => panic!("unimplemented type `{v}`"),
+		}
+	}
+
+	fn clone(&self) -> RESPMsg<'static> {
+		match self {
+			RESPMsg::SimpleString(v) => RESPMsg::SimpleString(v.clone()),
+			RESPMsg::BulkString(v) => RESPMsg::BulkString(v.clone()),
+			RESPMsg::Array(v) => {
+				let arr: RESPArray<'static> = v.clone_manually();
+				RESPMsg::Array(arr)
+			}
+			RESPMsg::Null => RESPMsg::Null,
 		}
 	}
 }
@@ -131,6 +200,10 @@ impl SimpleString<'_> {
 		let str = std::str::from_utf8(&(*input)[1..str_end_idx]).unwrap();
 		*input = &((*input)[(str_end_idx + 2)..]);
 		SimpleString(str.into())
+	}
+
+	fn clone(&self) -> SimpleString<'static> {
+		SimpleString(Cow::Owned(self.0.to_string()))
 	}
 }
 
@@ -163,6 +236,10 @@ impl BulkString<'_> {
 		);
 		*input = &(*input)[(len_end_idx + 2 + length + 2)..];
 		BulkString(data.into())
+	}
+
+	fn clone(&self) -> BulkString<'static> {
+		BulkString(Cow::Owned(self.0.to_string()))
 	}
 }
 
@@ -198,6 +275,15 @@ impl RESPArray<'_> {
 		*input = &(*input)[input_len_end_idx + 2..];
 		for _ in 0..length {
 			let msg: RESPMsg<'a> = RESPMsg::decode(input);
+			vec.push(msg);
+		}
+		RESPArray(vec)
+	}
+
+	fn clone_manually(&self) -> RESPArray<'static> {
+		let mut vec: Vec<RESPMsg<'static>> = Vec::with_capacity(self.0.len());
+		for x in self.0.iter() {
+			let msg: RESPMsg<'static> = x.clone();
 			vec.push(msg);
 		}
 		RESPArray(vec)
